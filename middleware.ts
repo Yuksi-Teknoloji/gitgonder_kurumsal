3c6ec2b54da74bfd71e4c0931b291761239a9be2
EmreKuzey04 <emrekuzey88@gmail.com>
2026-01-27 18:44:29 +0300

    bug fix try

--- middleware.ts
+++ middleware.ts
@@ -60,6 +60,7 @@ export async function middleware(request: NextRequest) {
     pathname.startsWith("/_next") ||
     pathname.startsWith("/api") ||
     pathname.startsWith("/static") ||
+    pathname.startsWith("/yuksi") ||
     pathname === "/" ||
     pathname.startsWith("/login") ||
     pathname.startsWith("/register")
